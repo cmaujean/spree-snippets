5bc7c95c45cf7c30fec79702a728605342f1be59
Eliot Sykes <eliotsykes@gmail.com>
2009-07-19 18:09:35 +0100

    Few tweaks: 1) raised errors explain error and include snippet name; 2) moved render_snippet helper method into the controller as edge spree was not picking up the SnippetHelper module; 3) extension now works with both gem and source installs of spree by using RAILS_ROOT or SPREE_ROOT to find the snippet wrapper _display.html.erb; 4) snippet_wrapper_path method added to allow developers to specify a template file that lives outside of the snippets extension.

--- app/helpers/snippet_helper.rb
+++ app/helpers/snippet_helper.rb
@@ -1,28 +1,2 @@
-require 'erb'
-
 module SnippetHelper
-  
-  # override snippet/_display.html.erb to change 
-  # the snippet wrapper
-  # 
-  # snippet can be something that responds to "slug" and "content", 
-  # or a slug, or an id
-  def render_snippet(snippet)
-    
-    # I hate stacks of elsifs. 
-    # But I can't think of a better way 
-    # to do this right now.   
-    if snippet.respond_to?("content")  
-      @snippet = snippet
-    elsif snippet.kind_of?(Fixnum)
-      @snippet = Snippet.find(snippet)
-    elsif snippet.kind_of?(String)
-      @snippet = Snippet.find_by_slug(snippet)
-    else
-      raise "Snippet not found"
-    end 
-
-    template = ERB.new File.read(File.expand_path(SPREE_ROOT + '/vendor/extensions/snippets/app/views/snippet/_display.html.erb'))
-    template.result(binding)
-  end
 end
--- snippets_extension.rb
+++ snippets_extension.rb
@@ -5,14 +5,6 @@ class SnippetsExtension < Spree::Extension
   
   # Developer will embed calls to render specific snippet slugs or ids
   # in any view. Admin will control content via /admin/snippets.
-
-
-  # Please use snippets/config/routes.rb instead for extension routes.
-
-  # def self.require_gems(config)
-  #   config.gem "gemname-goes-here", :version => '1.2.3'
-  # end
-  
   def activate
 
     Admin::BaseController.class_eval do 
@@ -24,7 +16,39 @@ class SnippetsExtension < Spree::Extension
     end
     
     Spree::BaseController.class_eval do
-      helper SnippetHelper
+
+      helper_method :render_snippet
+      
+      # snippet can be something that responds to "slug" and "content", 
+      # or a slug, or an id
+      def render_snippet(snippet)
+        if snippet.respond_to?('content')  
+          @snippet = snippet
+        elsif snippet.kind_of?(Fixnum)
+          @snippet = Snippet.find(snippet)
+        elsif snippet.kind_of?(String)
+          @snippet = Snippet.find_by_slug(snippet)
+        else
+          raise "Unable to handle snippet '#{snippet}'"
+        end 
+        raise "Snippet '#{snippet}' not found" if @snippet.nil?
+        template = ERB.new File.read(File.expand_path(snippet_wrapper_absolute_path))
+        template.result(binding)
+      end
+      
+      private
+      # Returns the location of the snippet wrapper, which is dependent on how spree is installed.
+      def snippet_wrapper_absolute_path
+        [SPREE_ROOT, RAILS_ROOT].each do |root|
+          absolute_path = root + snippet_wrapper_path
+          return absolute_path if File.exists?(absolute_path)
+        end
+        raise "Unable to find snippet wrapper using path '#{snippet_wrapper_path}'"        
+      end
+      # Override the location returned by this method if you want your own snippet wrapper template
+      def snippet_wrapper_path
+        '/vendor/extensions/snippets/app/views/snippet/_display.html.erb'
+      end
     end
 
   end
