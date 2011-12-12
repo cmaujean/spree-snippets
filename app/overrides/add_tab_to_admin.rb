41079e19be3dbda0866a675846794d9144cf6186
Kirillov Alexander <saratovsource@gmail.com>
2011-12-12 21:59:17 +0300

    Fix tabs insertion

--- app/overrides/add_tab_to_admin.rb
+++ app/overrides/add_tab_to_admin.rb
@@ -1,5 +1,4 @@
 Deface::Override.new(:virtual_path => "layouts/admin",
                      :name => "add_snippets_tab_to_admin",
-                     :insert_after => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:snippets) %>",
-                     :disabled => false)
+                     :insert_after => "[data-hook='admin_tabs']",
+                     :text => "<%= tab(:snippets) %>")
